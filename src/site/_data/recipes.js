66cc7f36299f2f435409203bcdc89fc2e5137c74
Simon Sinclair <simon@simonsinclair.com>
2020-02-23 17:20:27 +0000

    Disable data fetching for /recipes

--- src/site/_data/recipes.js
+++ src/site/_data/recipes.js
@@ -1,10 +1,10 @@
-const got = require('got');
+// const got = require('got');
 
-module.exports = async () => {
-  try {
-    const { body } = await got('https://api.thedailydietitian.co.uk/recipes');
-    return JSON.parse(body);
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+// module.exports = async () => {
+//   try {
+//     const { body } = await got('https://api.thedailydietitian.co.uk/recipes');
+//     return JSON.parse(body);
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
