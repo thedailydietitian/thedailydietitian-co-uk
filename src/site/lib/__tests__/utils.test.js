2985fc822c7a9b34ec27845b83fb0cf5f83f23f4
Simon Sinclair <simon@simonsinclair.com>
2020-10-11 15:36:22 +0100

    Remove site/lib

--- src/site/lib/__tests__/utils.test.js
+++
@@ -1,8 +0,0 @@
-const { decodeHexString } = require('../utils');
-
-describe('decodeHexString is a function that', () => {
-  it('decodes a hexadecimal string', () => {
-    const hexString = '48656C6C6F2C20576F726C6421';
-    expect(decodeHexString(hexString)).toEqual('Hello, World!');
-  });
-});
