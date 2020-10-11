c8e9c685b4f5b2bdbe1af0eb2dffed1c97e61a95
Simon Sinclair <simon@simonsinclair.com>
2020-10-11 12:55:55 +0100

    Remove holding page

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ const Terser = require('terser');
 
 module.exports = config => {
   config.addLayoutAlias('base', 'layouts/base.njk');
-  config.addLayoutAlias('holding', 'layouts/holding.njk');
   config.addLayoutAlias('recipe', 'layouts/recipe.njk');
 
   config.addPassthroughCopy('src/site/admin');
