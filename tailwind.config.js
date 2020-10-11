1d1ed80fb9c0653518f8247a5312c8a10571f3e9
Simon Sinclair <simon@simonsinclair.com>
2020-10-11 23:26:55 +0100

    Add Noto Serif Web font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   },
   purge: ['./src/**/*.njk'],
   theme: {
+    fontFamily: {
+      serif: ['Noto Serif', 'serif'],
+    },
+    fontSize: {
+      tiny: '0.667rem',
+      base: '1rem',
+      lg: '1.5rem',
+      xl: '2.25rem',
+    },
     screens: {
       sm: '640px',
       md: '768px',
